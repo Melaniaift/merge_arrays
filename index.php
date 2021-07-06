e93e8afc3c98e002a448d1e1572f54d7b8ea50bc
Melaniaift <ioanamelany99@yahoo.com>
2021-07-06 18:03:24 +0300

    merge

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Gestionarea array-urilor numerice </h1>
+        <ol>
+            <li><a href="merge.php">Array_merge()</a></li>
+        </ol>
+    </body>
+</html>
---
+++ merge.php
@@ -0,0 +1,12 @@
+<?php
+
+$first=array("a","b","c");
+$second=array(1,2,3);
+$third=array_merge($first,$second);
+foreach($third as $val)
+{
+    print $val."<BR>";
+}
+?>
+<br/><br/>
+<a href="index.php"> Back</a>
